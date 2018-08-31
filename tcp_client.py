aac3e766d1e7f05eeaaa5b06adabb78ad9347ece
GitUser004 <33181675+GitUser004@users.noreply.github.com>
2018-08-31 21:41:02 +0800

    tcp_client

---
+++ tcp_client.py
@@ -0,0 +1,22 @@
+from socket import *
+
+HOST = "10.4.211.37" #localhost
+PORT = 21567
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input(">")
+    if not data:
+        break
+    tcpCliSock.send(data.encode())
+    data = tcpCliSock.recv(BUFSIZE).decode()
+    if not data:
+        break
+    print(data)
+
+tcpCliSock.close()
+

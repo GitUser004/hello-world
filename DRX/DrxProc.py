1a254bbbacf6362a829f9692ea1eac03ffe9910b
郭宁10224316 <guo.ning3@zte.com.cn>
2019-08-09 22:17:36 +0800

    删除重复代码

--- DRX/DrxProc.py
+++ DRX/DrxProc.py
@@ -28,77 +28,31 @@ dlRetxList=[('0', '0', '0', '0')]
 dlRetxTmpList=[('0', '0', '0', '0')]
 
 
-def ProcOnduration(line):
-    global ondurationList,ondurationTmpList
-    mo = ondurationRegex.search(line)
+def ProcFileActivityRange(line,Regex,list,tmpList):
+    mo = Regex.search(line)
     if mo != None:
         # print(mo.groups())
         value = mo.groups()
-        if int(value[0]) < int(ondurationList[-1][0])//2:
-            if int(value[0])*20+int(value[1])<int(ondurationTmpList[-1][0])*20+int(ondurationTmpList[-1][1]):
-                ondurationTmpList.pop()
-            ondurationTmpList.append(value)
+        if int(value[0]) < int(list[-1][0])//2:
+            if int(value[0])*20+int(value[1])<int(tmpList[-1][0])*20+int(tmpList[-1][1]):
+                tmpList.pop()
+            tmpList.append(value)
         elif int(value[0])*20+int(value[1])<20480:
-            if int(value[0])*20+int(value[1])<int(ondurationList[-1][0])*20+int(ondurationList[-1][1]):
-                ondurationList.pop()
-            ondurationList.append(value)
-
-def ProcInactivity(line):
-    global inactivityList,inactivityTmpList
-    mo = inActivityRegex.search(line)
-    if mo != None:
-        # print(mo.groups())
-        value = mo.groups()
-        if int(value[0]) < int(inactivityList[-1][0])//2:
-            if int(value[0])*20+int(value[1])<int(inactivityTmpList[-1][0])*20+int(inactivityTmpList[-1][1]):
-                inactivityTmpList.pop()
-            inactivityTmpList.append(value)
-        elif int(value[0])*20+int(value[1])<20480:
-            if int(value[0])*20+int(value[1])<int(inactivityList[-1][0])*20+int(inactivityList[-1][1]):
-                inactivityList.pop()
-            inactivityList.append(value)
-
-def ProcUlRetx(line):
-    global ulRetxList, ulRetxTmpList
-    mo = ulRetxRegex.search(line)
-    if mo != None:
-        # print(mo.groups())
-        value = mo.groups()
-        if int(value[0]) < int(ulRetxList[-1][0]) // 2:
-            if int(value[0]) * 20 + int(value[1]) < int(ulRetxTmpList[-1][0]) * 20 + int(ulRetxTmpList[-1][1]):
-                ulRetxTmpList.pop()
-            ulRetxTmpList.append(value)
-        elif int(value[0]) * 20 + int(value[1]) < 20480:
-            if int(value[0]) * 20 + int(value[1]) < int(ulRetxList[-1][0]) * 20 + int(ulRetxList[-1][1]):
-                ulRetxList.pop()
-            ulRetxList.append(value)
-
-def ProcDlRetx(line):
-    global dlRetxList, dlRetxTmpList
-    mo = dlRetxRegex.search(line)
-    if mo != None:
-        # print(mo.groups())
-        value = mo.groups()
-        if int(value[0]) < int(dlRetxList[-1][0]) // 2:
-            if int(value[0]) * 20 + int(value[1]) < int(dlRetxTmpList[-1][0]) * 20 + int(dlRetxTmpList[-1][1]):
-                dlRetxTmpList.pop()
-            dlRetxTmpList.append(value)
-        elif int(value[0]) * 20 + int(value[1]) < 20480:
-            if int(value[0]) * 20 + int(value[1]) < int(dlRetxList[-1][0]) * 20 + int(dlRetxList[-1][1]):
-                dlRetxList.pop()
-            dlRetxList.append(value)
+            if int(value[0])*20+int(value[1])<int(list[-1][0])*20+int(list[-1][1]):
+                list.pop()
+            list.append(value)
 
 def DrawList(type,list,value,activeY,activeValue,ax):
     if len(list)>1:
         Y = [value]*1024*20
-        value_1 = value + 0.1
+        value_Y = value + 0.1
         activeYValue = activeValue + 0.1
         del list[0]
         for startFrame, startSlot, endFrame, endSlot in list:
             # print(startFrame,startSlot,endFrame,endSlot)
             startIndex = int(startFrame) * SLOT_PER_FRAME + int(startSlot)
             endIndex = int(endFrame) * SLOT_PER_FRAME + int(endSlot)
-            Y[startIndex:endIndex+1] = [value_1]*(endIndex+1-startIndex)
+            Y[startIndex:endIndex+1] = [value_Y]*(endIndex+1-startIndex)
             activeY[startIndex:endIndex+1] = [activeYValue]*(endIndex+1-startIndex)
         line = ax.plot(Y,linewidth=0.5, label=type)
         legend = ax.legend(loc='upper left', shadow=True, fontsize='medium')
@@ -113,64 +67,11 @@ def Draw(list1, value1, list2, value2, list3, value3,list4,value4,value):
     ax = fig.subplots()
 
     activeY = [value]*1024*20
-    value_1 = value + 0.1
 
     DrawList("Onduration",list1,value1,activeY,value,ax)
-    DrawList("InActivity", list1, value1, activeY, value, ax)
-
-    # if len(list1)>1:
-    #     Y = [value1]*1024*20
-    #     value1_1 = value1 + 0.1
-    #     del list1[0]
-    #     for startFrame, startSlot, endFrame, endSlot in list1:
-    #         # print(startFrame,startSlot,endFrame,endSlot)
-    #         startIndex = int(startFrame) * SLOT_PER_FRAME + int(startSlot)
-    #         endIndex = int(endFrame) * SLOT_PER_FRAME + int(endSlot)
-    #         Y[startIndex:endIndex+1] = [value1_1]*(endIndex+1-startIndex)
-    #         activeY[startIndex:endIndex+1] = [value_1]*(endIndex+1-startIndex)
-    #     line = ax.plot(Y,linewidth=0.5, label="Onduration")
-    #     legend = ax.legend(loc='upper left', shadow=True, fontsize='medium')
-    #     # line = ax.plot(Y,'o',linewidth=0.5,label="Onduration",markersize = 1)
-    #     # plt.setp(line, linewidth=4)
-
-    if len(list2)>1:
-        Y = [value2]*1024*20
-        value2_2 = value2 + 0.1
-        del list2[0]
-        for startFrame, startSlot, endFrame, endSlot in list2:
-            # print(startFrame,startSlot,endFrame,endSlot)
-            startIndex = int(startFrame) * SLOT_PER_FRAME + int(startSlot)
-            endIndex = int(endFrame) * SLOT_PER_FRAME + int(endSlot)
-            Y[startIndex:endIndex+1] = [value2_2]*(endIndex+1-startIndex)
-            activeY[startIndex:endIndex+1] = [value_1]*(endIndex+1-startIndex)
-        line = ax.plot(Y,linewidth=0.5,label="InActivity")
-        legend = ax.legend(loc='upper left', shadow=True, fontsize='medium')
-
-    if len(list3)>1:
-        Y = [value3]*1024*20
-        value3_3 = value3 + 0.1
-        del list3[0]
-        for startFrame, startSlot, endFrame, endSlot in list3:
-            # print(startFrame,startSlot,endFrame,endSlot)
-            startIndex = int(startFrame) * SLOT_PER_FRAME + int(startSlot)
-            endIndex = int(endFrame) * SLOT_PER_FRAME + int(endSlot)
-            Y[startIndex:endIndex+1] = [value3_3]*(endIndex+1-startIndex)
-            activeY[startIndex:endIndex+1] = [value_1]*(endIndex+1-startIndex)
-        line = ax.plot(Y,linewidth=0.5,label="UlRetx")
-        legend = ax.legend(loc='upper left', shadow=True, fontsize='medium')
-
-    if len(list4)>1:
-        Y = [value4]*1024*20
-        value4_4 = value4 + 0.1
-        del list4[0]
-        for startFrame, startSlot, endFrame, endSlot in list4:
-            # print(startFrame,startSlot,endFrame,endSlot)
-            startIndex = int(startFrame) * SLOT_PER_FRAME + int(startSlot)
-            endIndex = int(endFrame) * SLOT_PER_FRAME + int(endSlot)
-            Y[startIndex:endIndex+1] = [value4_4]*(endIndex+1-startIndex)
-            activeY[startIndex:endIndex+1] = [value_1]*(endIndex+1-startIndex)
-        line = ax.plot(Y,linewidth=0.5,label="DlRetx")
-        legend = ax.legend(loc='upper left', shadow=True, fontsize='medium')
+    DrawList("InActivity", list2, value2, activeY, value, ax)
+    DrawList("UlRetx", list3, value3, activeY, value, ax)
+    DrawList("DlRetx", list4, value4, activeY, value, ax)
 
     line = ax.plot(activeY, linewidth=0.5, label="Active")
     legend = ax.legend(loc='upper left', shadow=True, fontsize='medium')
@@ -190,29 +91,45 @@ def ClearListData():
     dlRetxList = [('0', '0', '0', '0')]
     dlRetxTmpList = [('0', '0', '0', '0')]
 
+def printList(textBrowser_output):
+    global ondurationList,inactivityList,ulRetxList,dlRetxList
+    print("onduration:", ondurationList)
+    print("inactivity:", inactivityList)
+    print("ulretx    :", ulRetxList)
+    print("dlretx    :", dlRetxList)
+    if textBrowser_output != None:
+        textBrowser_output.append("onduration:" + str(ondurationList))
+        textBrowser_output.append("inactivity:" + str(inactivityList))
+        textBrowser_output.append("ulretx    :" + str(ulRetxList))
+        textBrowser_output.append("dlretx    :" + str(dlRetxList))
+
+def printTmpList(textBrowser_output):
+    global ondurationTmpList,inactivityTmpList,ulRetxTmpList,dlRetxTmpList
+    print("ondurationTmp:", ondurationTmpList)
+    print("inactivityTmp:", inactivityTmpList)
+    print("ulretxTmp    :", ulRetxTmpList)
+    print("dlretxTmp    :", dlRetxTmpList)
+    if textBrowser_output != None:
+        textBrowser_output.append("ondurationTmp:" + str(ondurationTmpList))
+        textBrowser_output.append("inactivityTmp:" + str(inactivityTmpList))
+        textBrowser_output.append("ulretxTmp    :" + str(ulRetxTmpList))
+        textBrowser_output.append("dlretxTmp    :" + str(dlRetxTmpList))
+
 def DrxFileParser(fileName,isDrawOnduration,isDrawInactivity,isDrawUlRetx,isDrawDlRetx,textBrowser_output):
     global ondurationList,ondurationTmpList,inactivityList,inactivityTmpList,ulRetxList,ulRetxTmpList,dlRetxList,dlRetxTmpList
     with open(fileName) as drxFile:
         for line in drxFile:
             if isDrawOnduration:
-                ProcOnduration(line)
+                ProcFileActivityRange(line,ondurationRegex,ondurationList,ondurationTmpList)
             if isDrawInactivity:
-                ProcInactivity(line)
+                ProcFileActivityRange(line, inActivityRegex, inactivityList, inactivityTmpList)
             if isDrawUlRetx:
-                ProcUlRetx(line)
+                ProcFileActivityRange(line,ulRetxRegex,ulRetxList,ulRetxTmpList)
             if isDrawDlRetx:
-                ProcDlRetx(line)
+                ProcFileActivityRange(line,dlRetxRegex,dlRetxList,dlRetxTmpList)
 
             if len(ondurationTmpList) > 8 or len(inactivityTmpList) > 8 or len(ulRetxTmpList) > 8 or len(dlRetxTmpList) > 8:
-                print("onduration:",ondurationList)
-                print("inactivity:",inactivityList)
-                print("ulretx    :",ulRetxList)
-                print("dlretx    :",dlRetxList)
-                if textBrowser_output != None:
-                    textBrowser_output.append("onduration:"+str(ondurationList))
-                    textBrowser_output.append("inactivity:" + str(inactivityList))
-                    textBrowser_output.append("ulretx    :" + str(ulRetxList))
-                    textBrowser_output.append("dlretx    :" + str(dlRetxList))
+                printList(textBrowser_output)
                 Draw(ondurationList, ONDURATION_VALUE, inactivityList, INACTIVITY_VALUE,ulRetxList,ULRETX_VALUE,dlRetxList,DLRETX_VALUE,ACTIVE_VALUE)
                 ondurationList = ondurationTmpList
                 ondurationTmpList = [('0', '0', '0', '0')]
@@ -225,30 +142,14 @@ def DrxFileParser(fileName,isDrawOnduration,isDrawInactivity,isDrawUlRetx,isDraw
 
 def DrxLeftDataProc(textBrowser_output):
     global ondurationList,ondurationTmpList,inactivityList,inactivityTmpList,ulRetxList,ulRetxTmpList,dlRetxList,dlRetxTmpList
-    print("onduration:", ondurationList)
-    print("inactivity:", inactivityList)
-    print("ulretx    :", ulRetxList)
-    print("dlretx    :", dlRetxList)
-    if textBrowser_output != None:
-        textBrowser_output.append("onduration:" + str(ondurationList))
-        textBrowser_output.append("inactivity:" + str(inactivityList))
-        textBrowser_output.append("ulretx    :" + str(ulRetxList))
-        textBrowser_output.append("dlretx    :" + str(dlRetxList))
+    printList(textBrowser_output)
     Draw(ondurationList, ONDURATION_VALUE, inactivityList, INACTIVITY_VALUE,ulRetxList,ULRETX_VALUE,dlRetxList,DLRETX_VALUE,ACTIVE_VALUE)
-    print("ondurationTmp:", ondurationTmpList)
-    print("inactivityTmp:", inactivityTmpList)
-    print("ulretxTmp    :", ulRetxTmpList)
-    print("dlretxTmp    :", dlRetxTmpList)
-    if textBrowser_output != None:
-        textBrowser_output.append("ondurationTmp:" + str(ondurationTmpList))
-        textBrowser_output.append("inactivityTmp:" + str(inactivityTmpList))
-        textBrowser_output.append("ulretxTmp    :" + str(ulRetxTmpList))
-        textBrowser_output.append("dlretxTmp    :" + str(dlRetxTmpList))
+    printTmpList(textBrowser_output)
     Draw(ondurationTmpList, ONDURATION_VALUE, inactivityTmpList, INACTIVITY_VALUE,ulRetxTmpList,ULRETX_VALUE,dlRetxTmpList,DLRETX_VALUE,ACTIVE_VALUE)
 
 if __name__ == "__main__":
     fileName = "log.txt"
-    DrxFileParser(fileName,True,False,False,True,None)
+    DrxFileParser(fileName,True,True,True,True,None)
     DrxLeftDataProc(None)
     plt.show()
 
--- DRX/test.py
+++ DRX/test.py
@@ -1,44 +1,24 @@
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+def read_letter(letters,already_letter):
+    while letters:
+        current_letter = letters.pop()
 
-class RadioDemo(QWidget):
-    def __init__(self,parent=None):
-        super(RadioDemo,self).__init__(parent)
-        #水平布局
-        layout=QHBoxLayout()
+        #模拟读取字母的过程
+        print("Printing letter:" + current_letter)
+        already_letter.append(current_letter)
+def show_already_letter(already_letter):
+    print("\nThe follwing letters has been read:")
+    for letter in already_letter:
+        print(letter)
+
+def aaa(list):
+    list[0]="123"
 
 
-        self.btn1=QRadioButton('Button1')
-        #默认选中btn1
-        self.btn1.setChecked(True)
-        #toggled信号与槽函数绑定
-        self.btn1.toggled.connect(lambda :self.btnstate(self.btn1))
-        layout.addWidget(self.btn1)
+letters = ['A','B','C','D']
+already_letter=[]
 
-        self.btn2 = QRadioButton('Button2')
-        self.btn2.toggled.connect(lambda: self.btnstate(self.btn2))
-        layout.addWidget(self.btn2)
+read_letter(letters,already_letter)
+show_already_letter(already_letter)
 
-        self.setLayout(layout)
-        self.setWindowTitle('RadioButton demo')
-
-    def btnstate(self,btn):
-    #输出按钮1与按钮2的状态，选中还是没选中
-        if btn.text()=='Button1':
-            if btn.isChecked()==True:
-                print(btn.text()+"is selected")
-            else:
-                print(btn.text()+"is deselected")
-
-        if btn.text()=="Button2":
-            if btn.isChecked() == True:
-                print(btn.text() + "is selected")
-            else:
-                print(btn.text() + "is deselected")
-if __name__ == '__main__':
-    app=QApplication(sys.argv)
-    radioDemo=RadioDemo()
-    radioDemo.show()
-    sys.exit(app.exec_())
+aaa(already_letter)
+show_already_letter(already_letter)
